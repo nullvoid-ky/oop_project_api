ea874738c8820357cb74035734d224fac28bc9d1
pawito236 <pawito236@gmail.com>
2024-02-27 18:26:20 +0700

    refactor(Chat)

--- routers/chat.py
+++ routers/chat.py
@@ -6,7 +6,6 @@ from internal.auth import Auth
 from models.account import AccountModel
 from utils.response import Responses 
 from models.message import MessageModel
-from main import controller
 
 responses = Responses()
 
@@ -17,16 +16,17 @@ router = APIRouter(
 
 @router.post("/talk")
 async def talking(body : MessageModel):
-    try:
-        controller.talk(body.sender_id, body.receiver_id, body.text)
-        return Responses.success_response_status(status.HTTP_200_OK)
-    except Exception as e:
-        return Responses.error_response_status(status.HTTP_400_BAD_REQUEST, e)
+    from main import controller
+    respond = controller.talk(body.sender_id, body.receiver_id, body.text)
+    if(respond):
+        return Responses.success_response_status(status.HTTP_200_OK, "Send message Success", None)
+    else:
+        return Responses.error_response_status(status.HTTP_400_BAD_REQUEST, "Send message Error", None)
     
 
 @router.get("/chat-history")
 async def get_chat_history_by_id(sender_id: str, receiver_id: str):
-
+    from main import controller
     all_chat_data = controller.retrieve_chat_log(sender_id, receiver_id)
     
     if(len(all_chat_data) != 0):
@@ -36,7 +36,7 @@ async def get_chat_history_by_id(sender_id: str, receiver_id: str):
     
 @router.get("/chat-room")
 async def get_chat_room_by_id(sender_id: str):
-
+    from main import controller
     all_chat_room = controller.retrieve_chat_room(sender_id)
     
     if(len(all_chat_room) != 0):
