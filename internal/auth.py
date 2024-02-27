ae965a898c9ffa0ca00ed19f20a258f3f7dc5ad6
Tamtikorn <gan_zaza_12@hotmail.com>
2024-02-27 18:21:25 +0700

    fixed: auth system fail is fixed

--- internal/auth.py
+++ internal/auth.py
@@ -1,8 +1,5 @@
-from internal.controller import Controller
 from internal.account import Account  
 import bcrypt 
-
-controller = Controller() 
 
 class Auth:
     def __init__(self, username: str, password: str) -> None:
@@ -10,15 +7,16 @@ class Auth:
         self.__password = password
     
     async def register(self) -> dict | None:
+        from main import controller
         account: Account = await controller.get_account_by_username(self.__user)
         if account == None:
-            # return None
             hashed_password = bcrypt.hashpw(self.__password.encode('utf-8'), bcrypt.gensalt())
             new_account: Account = controller.add_account(self.__user, str(hashed_password))
             return new_account.get_account_details()
         return None
 
     async def login(self):
+        from main import controller
         account: Account = await controller.get_account_by_username(self.__user)
         if account == None:
             return None
