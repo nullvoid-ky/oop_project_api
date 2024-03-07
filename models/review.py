edf849093c1e766fd63e4c22aec32c545f397814
kanyok <134153117+nullvoid-ky@users.noreply.github.com>
2024-03-07 13:33:15 +0700

    feat(review): add rout for review

--- models/review.py
+++ models/review.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel
 
-class ReviewModel(BaseModel):
+class ReviewCreation(BaseModel):
     customer_id : str
     mate_id : str
     message : str
     star : int
+
+class ReviewDeletion(BaseModel):
+    mate_id : str
+    review_id : str
+
+class ReviewRead(BaseModel):
+    mate_id : str
