5b9b9a31f95d72da022653ec600dd2237d238721
Tamtikorn <gan_zaza_12@hotmail.com>
2024-03-12 11:57:40 +0700

    Add date import to mate.py

--- routers/mate.py
+++ routers/mate.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status, Body
-from datetime import datetime
+from datetime import datetime, date
 
 from models.review import *
 from models.availability import AvailabilityModel
